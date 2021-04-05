0d99e4de88ee3c458a90517b6fa621c977a53408
umairarshad2663 <80207197+umairarshad2663@users.noreply.github.com>
2021-04-05 10:12:04 +0500

    Add files via upload

---
+++ LAB6/Caculator.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Assignment;
+
+/**
+ *
+ * @author Majid
+ */
+public class Caculator {
+    public static int sum(int x, int y){//for sum of numbers
+        return (x+y);
+    }
+    public static int multi(int x, int y){//for product of numbers
+        return (x*y);
+    }
+    public static double div(double x, double y){//for division of numbers
+        return (x/y);
+    }
+    
+     public static int mod(int x, int y){//for modulus of numbers
+        return (x%y);
+    }
+     public static double sin(double x){
+        return (Math.sin(x));
+    }
+     public static double cos(double x){
+        return (Math.cos(x));
+    }
+     public static double tan(double x){
+        return (Math.tan(x));
+    }
+}
+
+  
